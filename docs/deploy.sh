0970e636706a0b62012642f085b2f5c72c5d26f5
“Alihossein <alih.sepahvand@gmail.com>
2025-09-14 07:43:18 -0600

    fix: update directory path in deploy script for correct project location

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
- cd /home/asepahvand/repos/innovoltive
+ cd /home/asepahvand/repos/website/innovoltive
  git pull && pnpm i && pnpm run build 
  sudo systemctl restart innovoltive.service
