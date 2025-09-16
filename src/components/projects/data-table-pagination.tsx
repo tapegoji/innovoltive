c501aab7c42da958fd0440d5cb41a0b3b533c00c
“Alihossein <alih.sepahvand@gmail.com>
2025-09-16 12:24:11 -0600

    fix: hide selected rows text and rows per page label on small screens for better responsiveness in DataTablePagination component

--- src/components/projects/data-table-pagination.tsx
+++ src/components/projects/data-table-pagination.tsx
@@ -23,13 +23,13 @@ export function DataTablePagination<TData>({
 }: DataTablePaginationProps<TData>) {
   return (
     <div className="flex items-center justify-between px-2">
-      <div className="text-muted-foreground flex-1 text-sm">
+      <div className="text-muted-foreground flex-1 text-sm hidden sm:block">
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
       </div>
-      <div className="flex items-center space-x-6 lg:space-x-8">
+      <div className="flex  space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
-          <p className="text-sm font-medium">Rows per page</p>
+          <p className="text-sm font-medium hidden sm:block">Rows per page</p>
           <Select
             value={`${table.getState().pagination.pageSize}`}
             onValueChange={(value) => {
