d3eff2e3aa1c0d0ac0ff9267bf1741165987426f
“Alihossein <alih.sepahvand@gmail.com>
2025-09-17 12:58:03 -0600

    feat: update Canvas component to improve mobile responsiveness with dynamic camera settings

--- src/app/(canvas)/canvas/canvas.tsx
+++ src/app/(canvas)/canvas/canvas.tsx
@@ -3,6 +3,7 @@
 import { Canvas as ThreeCanvas } from '@react-three/fiber'
 import { Edges, GizmoHelper, GizmoViewport, OrbitControls } from '@react-three/drei'
 import { ControlPanel } from '@/components/canvas/control-panel'
+import { useIsMobile } from '@/hooks/use-mobile'
 
 function Box() {
   return (
@@ -20,18 +21,22 @@ interface CanvasProps {
 }
 
 export default function Canvas({ projectName, projectHash }: CanvasProps) {
+  const isMobile = useIsMobile()
+  const zoom = isMobile ? 50 : 75
+  const gizmoScale: [number, number, number] = isMobile ? [30, 30, 30] : [40, 40, 40]
+  const gizmoMargin: [number, number] = isMobile ? [40, 60] : [60, 80]
   return (
     <div className="w-full h-[calc(100vh-2rem-1px)]">
       <ControlPanel projectName={projectName} projectHash={projectHash} />
       <ThreeCanvas
         orthographic
-        camera={{ position: [10, 10, 10], zoom: 75 }}
+        camera={{ position: [10, 10, 10], zoom }}
       >
         {/* <ambientLight intensity={1} /> */}
         {/* <pointLight position={[10, 10, 10]} /> */}
-        <Box />
-        <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
-          <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="white" />
+        {/* <Box /> */}
+        <GizmoHelper alignment="bottom-right" margin={gizmoMargin}>
+          <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="white" scale={gizmoScale} />
         </GizmoHelper>
         <OrbitControls enableDamping={false} makeDefault />
       </ThreeCanvas>
